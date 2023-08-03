853c417a72f163164f3dc978cdba4c3ee632b9b0
Allan Simoyi <allansimoyi@gmail.com>
2023-08-03 13:23:21 +0200

    Correct toolbar padding

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -27,7 +27,7 @@ export function Toolbar(props: ToolbarProps) {
           <ProgressBar />
         </div>
       )}
-      <div className="flex flex-col items-stretch p-4">
+      <div className="flex flex-col items-stretch px-2 py-4">
         <CenteredView>
           <div className="flex flex-row items-stretch justify-center">
             <div className="flex flex-col justify-center">
