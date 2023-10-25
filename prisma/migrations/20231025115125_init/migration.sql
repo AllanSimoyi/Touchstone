4c8922688be4335fc9d78d4a6920e2da0142663d
Allan Simoyi <allansimoyi@gmail.com>
2023-10-25 13:52:00 +0200

    Add new migrations

---
+++ prisma/migrations/20231025115125_init/migration.sql
@@ -0,0 +1,335 @@
+-- CreateTable
+CREATE TABLE `support_job` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `accountId` INTEGER NOT NULL,
+    `clientStaffName` VARCHAR(100) NOT NULL,
+    `supportPersonId` INTEGER NOT NULL,
+    `supportType` VARCHAR(500) NOT NULL,
+    `status` VARCHAR(100) NOT NULL,
+    `enquiry` TEXT NOT NULL,
+    `actionTaken` TEXT NOT NULL,
+    `charge` DECIMAL(19, 2) NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `support_job_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `supportJobId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `accountNumber` VARCHAR(20) NOT NULL,
+    `companyName` VARCHAR(200) NOT NULL,
+    `tradingAs` VARCHAR(200) NOT NULL,
+    `formerly` VARCHAR(200) NOT NULL,
+    `groupId` INTEGER NULL,
+    `areaId` INTEGER NULL,
+    `sectorId` INTEGER NULL,
+    `vatNumber` VARCHAR(200) NOT NULL,
+    `otherNames` VARCHAR(200) NOT NULL,
+    `description` VARCHAR(1600) NOT NULL,
+    `actual` INTEGER NOT NULL,
+    `reason` VARCHAR(500) NOT NULL,
+    `statusId` INTEGER NULL,
+    `contractNumber` VARCHAR(30) NOT NULL,
+    `dateOfContract` DATETIME(3) NULL,
+    `licenseId` INTEGER NULL,
+    `licenseDetailId` INTEGER NULL,
+    `addedPercentage` INTEGER NOT NULL,
+    `gross` DECIMAL(19, 2) NOT NULL,
+    `net` DECIMAL(19, 2) NOT NULL,
+    `vat` DECIMAL(19, 2) NOT NULL,
+    `comment` VARCHAR(1600) NOT NULL,
+    `ceoName` VARCHAR(50) NOT NULL,
+    `ceoEmail` VARCHAR(50) NOT NULL,
+    `ceoPhone` VARCHAR(40) NOT NULL,
+    `ceoFax` VARCHAR(20) NOT NULL,
+    `physicalAddress` VARCHAR(500) NOT NULL,
+    `telephoneNumber` VARCHAR(40) NOT NULL,
+    `faxNumber` VARCHAR(40) NOT NULL,
+    `cellphoneNumber` VARCHAR(40) NOT NULL,
+    `accountantName` VARCHAR(100) NOT NULL,
+    `accountantEmail` VARCHAR(100) NOT NULL,
+    `boxCityId` INTEGER NULL,
+    `boxNumber` VARCHAR(200) NOT NULL,
+    `boxArea` VARCHAR(200) NOT NULL,
+    `deliveryCityId` INTEGER NULL,
+    `deliveryAddress` VARCHAR(300) NOT NULL,
+    `deliverySuburb` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `account_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `accountId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `areas` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `areas_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `area_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `areaId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cities` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `cities_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `city_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `cityId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `databasez` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `accountId` INTEGER NOT NULL,
+    `databaseName` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `database_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `databaseId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `groupz` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `groupz_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `group_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `groupId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `licensedetails` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(500) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `licensedetails_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `licensedetails_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `licenseDetailId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `licenses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `basicUsd` DECIMAL(19, 2) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `licenses_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `license_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `licenseId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `operatorz` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `accountId` INTEGER NOT NULL,
+    `operatorName` VARCHAR(30) NOT NULL,
+    `operatorEmail` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `operator_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `operatorId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sectors` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `sectors_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sector_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `sectorId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `statuses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `statuses_identifier_key`(`identifier`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `status_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `statusId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(40) NOT NULL,
+    `password` TEXT NOT NULL,
+    `accessLevel` VARCHAR(40) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `recordId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `details` TEXT NOT NULL,
+    `kind` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
